b8b60745231389fcbd0a5fcdacb752b82a8cad1f
THEGAMER221148 <weststefany@icloud.com>
2025-05-21 12:44:10 -0700

    added cool font

--- main.js
+++ main.js
@@ -39,15 +39,15 @@ function gameLoop() {
 
 gameLoop();
 
-new DialogueBox("Hello, welcome to my cool dialogue system! Press enter to advance", "white", "sans-serif", 20, function(){
+new DialogueBox("Hello, welcome to my cool dialogue system! Press enter to advance", "white", "Tiny5", 20, function(){
 
-new DialogueBox("With this engine, you can change the text's:", "white", "sans-serif", 20, function(){
+new DialogueBox("With this engine, you can change the text's:", "white", "Tiny5", 20, function(){
 
-new DialogueBox("color,", "lime", "sans-serif", 20, function(){
+new DialogueBox("color,", "lime", "Tiny5", 20, function(){
 
 new DialogueBox("font,", "white", "papyrus", 20, function(){
 
-new DialogueBox("speed,", "red", "sans-serif", 100, function(){
+new DialogueBox("speed,", "red", "Tiny5", 100, function(){
 
 let then = Date.now();
 
@@ -64,23 +64,23 @@ function funny(){
 
 funny();
 
-new DialogueBox("and even what happens when the user closes the pop-up!", "blue", "sans-serif", 30, function(){
+new DialogueBox("and even what happens when the user closes the pop-up!", "blue", "Tiny5", 30, function(){
 
-new DialogueBox("You can also stack dialogue boxes, but I don't reccomend it.", "white", "sans-serif", 20, function(){
+new DialogueBox("You can also stack dialogue boxes, but I don't reccomend it.", "white", "Tiny5", 20, function(){
 
-new DialogueBox("It can be useful for", "white", "sans-serif", 20, function(){});
+new DialogueBox("It can be useful for", "white", "Tiny5", 20, function(){});
 setTimeout(() => {
-    new DialogueBox("cutscenes", "white", "sans-serif", 20, function(){});
+    new DialogueBox("cutscenes", "white", "Tiny5", 20, function(){});
     setTimeout(() => {
-        new DialogueBox("like", "white", "sans-serif", 20, function(){});
+        new DialogueBox("like", "white", "Tiny5", 20, function(){});
         setTimeout(() => {
-            new DialogueBox("this", "white", "sans-serif", 20, function(){
+            new DialogueBox("this", "white", "Tiny5", 20, function(){
 
-new DialogueBox("Anyway, I hope this gets put to good use", "white", "sans-serif", 20, function(){
+new DialogueBox("Anyway, I hope this gets put to good use", "white", "Tiny5", 20, function(){
 
-new DialogueBox("It's versitile, straightforward, and pretty bug-free", "white", "sans-serif", 20, function(){
+new DialogueBox("It's versitile, straightforward, and pretty bug-free", "white", "Tiny5", 20, function(){
 
-new DialogueBox("bye!", "white", "sans-serif", 20);
+new DialogueBox("bye!", "white", "Tiny5", 20);
 });
 });
 });
