77defec5ceee8cdd53a556b3d808db80c5826b84
THEGAMER221148 <weststefany@icloud.com>
2025-05-27 14:23:51 -0700

    got it tworking again

--- edging/Level.js
+++ edging/Level.js
@@ -1,17 +1,19 @@
 export default class Level {
-    constructor(backgroundImage, characters){
+    constructor(backgroundImage, backgroundScale, characters, currentCamera){
         this.bg = new Image();
         this.bg.src = backgroundImage;
+        this.scale = backgroundScale;
+        this.cam = currentCamera;
         this.chars = characters;
         this.canvas = document.getElementById("gameCanvas");
         this.ctx = document.getElementById("gameCanvas").getContext("2d");
     }
 
     render(){
-        this.ctx.drawImage(this.bg, 100, 100);
+        this.ctx.drawImage(this.bg, window.innerWidth/2-this.cam.position.x, this.cam.position.y-window.innerHeight/2, this.bg.width * this.scale, this.bg.height * this.scale);
         this.chars.forEach((item) => {
-            item.element.style.left = item.position.x + "px";
-            item.element.style.bottom = item.position.y + "px";
+            item.element.style.left = item.position.x - this.cam.position.x + window.innerWidth/2 + "px";
+            item.element.style.bottom = item.position.y - this.cam.position.y + window.innerHeight/2 + "px";
             item.element.style.width = item.size.x + "px";
             item.element.style.height = item.size.y + "px";
         });
